a8e42d0b23dcd2935a9b2e7d841a6a74af3ab8b6
ChenYing Kuo <evshary@gmail.com>
2024-07-10 13:50:37 +0800

    Move examples back to up-transport-zenoh-rust.
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

---
+++ examples/rpc_client.rs
@@ -0,0 +1,47 @@
+/********************************************************************************
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+mod common;
+
+use std::str::FromStr;
+use up_rust::{RpcClient, UMessageBuilder, UPayloadFormat, UUri};
+use up_transport_zenoh::UPClientZenoh;
+
+#[tokio::main]
+async fn main() {
+    // initiate logging
+    UPClientZenoh::try_init_log_from_env();
+
+    println!("uProtocol RPC client example");
+    let rpc_client = UPClientZenoh::new(common::get_zenoh_config(), String::from("rpc_client"))
+        .await
+        .unwrap();
+
+    // create uuri
+    let src_uuri = UUri::from_str("//rpc_client/1/1/0").unwrap();
+    let sink_uuri = UUri::from_str("//rpc_server/1/1/1").unwrap();
+
+    // create uPayload
+    let data = String::from("GetCurrentTime");
+    let umsg = UMessageBuilder::request(sink_uuri.clone(), src_uuri.clone(), 1000)
+        .build_with_payload(data, UPayloadFormat::UPAYLOAD_FORMAT_TEXT)
+        .unwrap();
+
+    // invoke RPC method
+    println!("Send request from {src_uuri} to {sink_uuri}");
+    let result = rpc_client.invoke_method(sink_uuri, umsg).await;
+
+    // process the result
+    let payload = result.unwrap().payload.unwrap();
+    let value = payload.into_iter().map(|c| c as char).collect::<String>();
+    println!("Receive {value}");
+}
---
+++ examples/subscriber.rs
@@ -0,0 +1,57 @@
+/********************************************************************************
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+mod common;
+
+use async_trait::async_trait;
+use std::{str::FromStr, sync::Arc};
+use tokio::time::{sleep, Duration};
+use up_rust::{UListener, UMessage, UStatus, UTransport, UUri};
+use up_transport_zenoh::UPClientZenoh;
+
+struct SubscriberListener;
+#[async_trait]
+impl UListener for SubscriberListener {
+    async fn on_receive(&self, msg: UMessage) {
+        let payload = msg.payload.unwrap();
+        let value = payload.into_iter().map(|c| c as char).collect::<String>();
+        let uri = msg.attributes.unwrap().source.unwrap().to_string();
+        println!("Receiving {value} from {uri}");
+    }
+    async fn on_error(&self, err: UStatus) {
+        panic!("Internal Error: {err:?}");
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    // initiate logging
+    UPClientZenoh::try_init_log_from_env();
+
+    println!("uProtocol subscriber example");
+    let subscriber = UPClientZenoh::new(common::get_zenoh_config(), String::from("subscriber"))
+        .await
+        .unwrap();
+
+    // create uuri
+    let uuri = UUri::from_str("//publisher/1/1/8001").unwrap();
+
+    println!("Register the listener...");
+    subscriber
+        .register_listener(&uuri, None, Arc::new(SubscriberListener {}))
+        .await
+        .unwrap();
+
+    loop {
+        sleep(Duration::from_millis(1000)).await;
+    }
+}
